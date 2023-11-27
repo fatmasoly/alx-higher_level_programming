5906ea3f05b17feeb37ea4257bbe3ed66609cb16
fatma <fatmacivil@hotmail.com>
2023-11-27 11:06:11 -0800

    fk

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,9 +1,10 @@
 import dis
 
+
 def magic_calculation(a, b):
     result = 98
     result += a ** b
     return result
 
-# Verify that the bytecode is equivalent
+
 print(dis.dis(magic_calculation))

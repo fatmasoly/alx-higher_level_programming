79f472d53fa88124d9043b4345efa085d6d55b2d
fatma <fatmacivil@hotmail.com>
2023-11-27 11:00:10 -0800

    tt

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,10 +1,4 @@
-import dis
-
-
+#!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 98
-    result += a ** b
-    return (result)
-
-
-print(dis.dis(magic_calculation))
+result = 98
+result += a ** b

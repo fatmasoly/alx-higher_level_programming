d967431deb999157cf984709a16c052a4aa9a395
fatma <fatmacivil@hotmail.com>
2023-11-28 10:27:55 -0800

    werwerwer

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-def uppercase(str):
-    for char in str:
-        if ord('a') <= ord(char) <= ord('z'):
-            print("{}".format(chr(ord(char) - 32)), end="")
-        else:
-            print("{}".format(char), end="")
-            print()
+def uppercase(s):
+
+    for char in s:
+        print("{}"
+              .format(chr(ord(char) - 32) if 'a' <= char <= 'z' else char),
+              end="")
+
+
+print()

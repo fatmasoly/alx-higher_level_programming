21bd1f6cb0a4d5dd7d93815c5fa22ec010d1094b
fatma <fatmacivil@hotmail.com>
2023-11-28 11:11:23 -0800

    vuuu

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 for char in range(122, 96, -1):
-    print("{:c}".format(char), end="")
-    char -= 32 if char % 2 == 0 else 0
+    print("{:c}".format(char - 32 if char % 2 == 0 else char), end="")
 print("")

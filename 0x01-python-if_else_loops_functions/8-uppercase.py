d759436aab0b302cdfeba979e6d5a626c2c899bf
fatma <fatmacivil@hotmail.com>
2023-11-28 10:34:01 -0800

    dsdsds

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
+def islower(c):
+
+    return ord('a') <= ord(c) <= ord('z')
+
+
 def uppercase(s):
 
     for char in s:
         print("{}"
-              .format(chr(ord(char) - 32) if 'a' <= char <= 'z' else char),
+              .format(chr(ord(char) - 32) if islower(char) else char),
               end="")
 
 

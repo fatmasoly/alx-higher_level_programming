f7cc7b25183a254aea763e28e95bd70201d57e4e
fatma <fatmacivil@hotmail.com>
2023-11-30 04:49:12 -0800

    bbbbb

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,12 @@
 if __name__ == "__main__":
     import sys
     argc = len(sys.argv) - 1
+    for n in range(argc):
+        print("{}: {}".format(n + 1, sys.argv[n + 1]))
     if argc == 0:
         print("0 arguments.")
     else:
-        print("{} arguments:".format(argc))
-        for n in range(argc):
-            print("{}: {}".format(n + 1, sys.argv[n + 1]))
+        if argc == 1:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(argc))

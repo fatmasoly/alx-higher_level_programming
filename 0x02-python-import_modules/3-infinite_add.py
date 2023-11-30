7266290db7aa86660a4809e96eec1e6c1f5358a1
fatma <fatmacivil@hotmail.com>
2023-11-30 06:42:28 -0800

    mkmk

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-import sys
-sum = 0
-for args in range(len(sys.argv) - 1):
-    sum += int(sys.argv[args + 1])
-print(sum)
+if __name__ == "__main__":
+    import sys
+
+    total_sum = 0
+
+    for args in range(len(sys.argv) - 1):
+        total_sum += int(sys.argv[args + 1])
+
+    print(total_sum)

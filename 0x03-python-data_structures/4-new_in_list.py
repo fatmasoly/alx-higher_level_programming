2e081ff82629136d6930c934a83f5cc6f6c63deb
fatma <fatmacivil@hotmail.com>
2023-12-02 10:54:56 -0800

    5th

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    cpy_list = my_list[:]
+    if idx < 0 or idx >= len(cpy_list):
+        return cpy_list
+    else:
+        cpy_list[idx] = element
+        return cpy_list

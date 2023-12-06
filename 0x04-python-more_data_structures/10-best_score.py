4263a480ffb5de8f56acafe20b1a4a3a4615edbc
fatma <fatmacivil@hotmail.com>
2023-12-06 05:39:54 -0800

    rtr

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,6 +4,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
-    best_sco = {key: value * 2 for key, value in a_dictionary.items()}
-
-    return best_sco
+        a = a_dictionary.copy()
+    for dic_key, dic_val in b.items():
+        a[dic_key] *= 2
+    return a

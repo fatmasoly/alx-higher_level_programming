bcb6cf2ae05c9f295131711d4e10680eb772fe8a
fatma <fatmacivil@hotmail.com>
2023-12-06 03:08:37 -0800

    3rdd

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,10 +2,11 @@
 
 def uniq_add(my_list=[]):
     if my_list is None:
-        return
+        return None
     uniq_int = set()
     for items in my_list:
         uniq_int.add(items)
-        sum_uniq = sum(uniq_int)
+
+    sum_uniq = sum(uniq_int)
 
     return sum_uniq

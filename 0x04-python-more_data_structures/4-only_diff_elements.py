32f4fe9e3f0255922445532a294ee07cc09f6f81
fatma <fatmacivil@hotmail.com>
2023-12-06 03:42:07 -0800

    fu

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -4,13 +4,4 @@ def only_diff_elements(set_1, set_2):
     if set_1 is None or set_2 is None:
         return set()
 
-    a = set()
-    for item in set_1:
-        if item not in set_2:
-            a.add(item)
-
-            for item in set_2:
-                if item not in set_1:
-                    a.add(item)
-
-    return a
+    return set_1 ^ set_2

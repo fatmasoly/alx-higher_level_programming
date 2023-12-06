7914d0f7f11e014b4a1f5d9b414964d69fda6124
fatma <fatmacivil@hotmail.com>
2023-12-06 06:45:12 -0800

    ffffff

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or not a_dictionary:
         return None
 
     best_key = max(a_dictionary, key=a_dictionary.get)

db35ec2f20dd417ea5353a89655ab2fd0ee7002f
fatma <fatmacivil@hotmail.com>
2023-12-06 03:05:48 -0800

    3rd

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    if my_list is None:
+        return
+    uniq_int = set()
+    for items in my_list:
+        uniq_int.add(items)
+        sum_uniq = sum(uniq_int)
+
+    return sum_uniq

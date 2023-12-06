49c3ac0ffcbee6ce0ffbafba57e9befd9bd0d53c
fatma <fatmacivil@hotmail.com>
2023-12-06 05:16:43 -0800

    8

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    if a_dictionary is None:
+        return
+
+    new_dict = {key: value * 2 for key, value in a_dictionary.items()}
+
+    return new_dict

48240353a7b9e8d414abac760073ffec107029b6
fatma <fatmacivil@hotmail.com>
2023-12-18 07:03:54 -0800

    fin

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,8 +6,8 @@ import sys
 def safe_print_integer_err(value):
     int_num = True
     try:
-        print("{:d}".format(value), end="")
+        print("{:d}".format(value))
     except Exception as e:
-        print("Exception:", e,  file=open("/dev/stderr", "w"))
+        print("Exception:", e, file=sys.stderr)
         int_num = False
     return int_num

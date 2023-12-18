2540423639213601ca579917011f784261b19fba
fatma <fatmacivil@hotmail.com>
2023-12-18 03:52:03 -0800

    gvb

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,4 +14,4 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
         finally:
             new_list.append(x)
-            return new_list
+    return new_list

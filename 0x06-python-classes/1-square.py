2c4eb70c9175cfa633fbef0956c6405c7bf225d1
fatma <fatmacivil@hotmail.com>
2023-12-19 03:15:53 -0800

    2nd

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" this is square module """
+
+
+class Square:
+    """ this is a square class"""
+    def __init__(self, size):
+        """ this is the attrebute
+            Args:
+                size: The first parameter.
+        """
+        self.__size = size

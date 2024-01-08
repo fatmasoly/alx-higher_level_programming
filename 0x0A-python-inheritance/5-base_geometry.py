6e70daacd9fae4f46f9997c49d3203c47edbd3c7
fatma <fatmacivil@hotmail.com>
2024-01-08 05:43:13 -0800

    55

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module: base_geometry
+
+This module defines the BaseGeometry class, which
+serves as a base class for geometry-related operations.
+
+Classes:
+- BaseGeometry
+    A simple base class for geometry operations.
+    It does not implement any specific methods.
+
+Note: This module provides a foundation for more
+specific geometry-related classes.
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class serves as a base class for
+    geometry-related operations.
+
+    Methods:
+    - No specific methods are implemented in the BaseGeometry class.
+    """
+
+    pass

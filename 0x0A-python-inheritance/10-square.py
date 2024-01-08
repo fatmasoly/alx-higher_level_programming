59bfb5412e705a81822f98a250342625a9d2ebba
fatma <fatmacivil@hotmail.com>
2024-01-08 09:05:29 -0800

    10th

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Module for the Rectangle class '''
+Rectangle = __import__('9-rectangle').Rectangle
+class Square(Rectangle):
+    def __init__(self, size):
+            def __init__(self, size):
+        '''Initializes new square instance '''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        '''Method calculates the area of square '''
+        return self.__size ** 2

d1b8bb45d82dfbebeb59f085913eb4c245249ffa
fatma <fatmacivil@hotmail.com>
2024-01-08 05:29:31 -0800

    6tt

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module provides functions for checking the type
+and class inheritance relationships of objects.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class; otherwise, False.
+
+    Parameters:
+    - obj: Any Python object
+        The object to check.
+    - a_class: class
+        The specified class to check against.
+
+    Returns:
+    - bool
+        True if the object is an instance of a class that inherited
+        from the specified class; otherwise, False.
+    """
+    return issubclass(type(obj), a_class)

4178e276cb4e8d3173dd1a295f73b32c88117503
root <root@DESKTOP-CTTDNQL.localdomain>
2024-01-09 18:23:13 +0300

    10th

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+This module about a class Student
+"""
+
+
+class Student:
+    """
+    A class Student that defines a student by: first_name, last_name, and age.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student object
+        with the provided information.
+
+        Parameters:
+        - first_name (str): The first name of the student.
+        - last_name (str): The last name of the student.
+        - age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+        def to_json(self, attrs=None):
+            """
+        Retrieves a dictionary representation of a Student instance.
+
+        Parameters:
+        - attrs (list, optional): A list of strings representing attribute
+        names to be included.If None, all attributes will be included.
+
+        Returns:
+        dict: A dictionary containing the selected
+        attributes of the Student instance.
+        """
+        if not isinstance(attrs, list):
+            return self.__dict__
+        selected = {}
+        for x in attrs:
+            if not isinstance(x, str):
+                return self.__dict__
+            else:
+                try:
+                    selected[x] = self.__getattribute__(x)
+                except AttributeError:
+                    pass
+        return selected

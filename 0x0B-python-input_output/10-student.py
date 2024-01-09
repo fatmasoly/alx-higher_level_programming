40dc18c5bd59877ae64f203dbcb9e612ad90f110
root <root@DESKTOP-CTTDNQL.localdomain>
2024-01-09 19:12:48 +0300

    100

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,51 +1,44 @@
 #!/usr/bin/python3
 """
-This module about a class Student
+This module provide a class Student.
 """
-
-
-from attr import attrs
 
 
 class Student:
     """
-    A class Student that defines a student with attr: first_name, last_name, and age.
+    A class representing a student with attr: first_name, last_name, and age.
     """
     def __init__(self, first_name, last_name, age):
         """
-        Initializes a new Student object
-        with the provided information.
+        Initialize a new Student object.
 
-        Parameters:
-        - first_name (str): The first name of the student.
-        - last_name (str): The last name of the student.
-        - age (int): The age of the student.
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-        def to_json(self, attrs=None):
-            """
-        Retrieves a dictionary representation of a Student instance.
-
-        Parameters:
-        - attrs (list, optional): A list of strings representing attribute
-        names to be included.If None, all attributes will be included.
+    def to_json(self, attrs=None):
+        """
+        Convert the Student object's attributes into a dictionary.
+        Args:
+            attrs (list): attr list.
 
         Returns:
-        dict: A dictionary containing the selected
-        attributes of the Student instance.
-            """
-            if not isinstance(attrs, list):
-                return self.__dict__
-        selected = {}
+            dict: A dictionary representation of the Student object's attr.
+        """
+        if not isinstance(attrs, list):
+            return self.__dict__
+        ans = {}
         for x in attrs:
             if not isinstance(x, str):
                 return self.__dict__
             else:
                 try:
-                    selected[x] = self.__getattribute__(x)
+                    ans[x] = self.__getattribute__(x)
                 except AttributeError:
                     pass
-        return selected
+        return ans

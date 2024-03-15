1f94a2c5f21e16b773d5b2d18a2af3db942b8f6d
root <root@DESKTOP-CTTDNQL.localdomain>
2024-03-15 10:21:40 +0300

    vvvv

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
 
+"""This script connects to a MySQL database and
+retrieves rows from the `states` table
+where the `name` column matches the provided argument.
+It takes command-line arguments for MySQL username, password,
+database name, and state name to search for.
+The script connects to a MySQL server running
+on localhost at port 3306.
+The retrieved rows are displayed in the console."""
 
-import MySQLdb
-from sys import argv
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-if __name__ == '__main__':
-
-
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
-
-    with db.cursor() as cur:
-        cur.execute("""SELECT * FROM states
-            WHERE name LIKE BINARY '{}'
-            ORDER BY states.id ASC""",{'name' : argv[4]})
-
-        rows = cur.fetchall()
-
-    if rows is not None:
-        for row in rows:
-            print(row)
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name"
+                " = %s ORDER BY id;", (sys.argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()

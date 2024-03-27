504c53694ed2eaa5679c04ef6acc61e5f0ab94b8
fatma <fatmacivil@hotmail.com>
2024-03-27 04:04:19 -0700

    102fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This Bash script that makes a request to 0.0.0.0:5000/catch_me
-curl -s -X PUT 0.0.0.0:5000/catch_me -d "user_id=98"
+curl -s -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"

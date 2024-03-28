4afe1352b05f065bf1624e73261dcd005ff40e9d
root <root@DESKTOP-CTTDNQL.localdomain>
2024-03-28 14:42:40 +0300

    eee

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,9 +5,9 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    req = requests.get(url)
 
+    req = requests.get(url)
     if req.status_code >= 400:
-        print("Error code:{}".format(req.status_code))
+        print("Error code: {}".format(req.status_code))
     else:
         print(req.text)

cbfb823b73e7c7b5dedfc6b73de6c660b3fe3467
root <root@DESKTOP-CTTDNQL.localdomain>
2024-03-29 01:12:58 +0300

    100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python script that takes 2 arguments"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+    response = requests.get(url)
+    commits = response.json()
+    try:
+        for x in range(10):
+            print("{}: {}".format(
+                commits[x].get("sha"),
+                commits[x].get("commit").get("author").get("name")))
+    except IndexError:
+            pass

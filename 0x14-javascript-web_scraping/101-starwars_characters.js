4ae5ba9ed4a1070795399fceb2b4039385925faf
Fatma <fatmacivil@hotmail.com>
2024-04-23 22:34:24 +0300

    ggg

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,19 +4,19 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, (err, response, body) => {
-  if (err) {
-    console.error(err);
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
   } else {
     const content = JSON.parse(body);
     const characters = content.characters;
     for (const character of characters) {
-      request.get(character, (err, response, body) => {
-        if (err) {
-          console.error(err);
+      request.get(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
         } else {
-          const name = JSON.parse(body).name;
-          console.log(name);
+          const names = JSON.parse(body);
+          console.log(names.name);
         }
       });
     }

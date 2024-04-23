8de27f6ca138dbbb14c6ac95b12524f9f051c2d9
Fatma <fatmacivil@hotmail.com>
2024-04-23 17:32:34 +0300

    fix pipe

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,14 +2,5 @@
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-request.get(url, function (error, response) {
-  if (error) {
-    console.log(error);
-  } else {
-    fs.writeFile('store.json', response.body, 'utf8', (err) => {
-      if (err) {
-        console.log(err);
-      }
-    });
-  }
-});
+const response = process.argv[3];
+request(url).pipe(fs.createWriteStream(response));

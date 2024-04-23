4514871947462f16eb1e9edeb711539c4bb91d2a
Fatma <fatmacivil@hotmail.com>
2024-04-23 22:58:03 +0300

    fix 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,25 +6,18 @@ const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request.get(url, (error, response, body) => {
-    if (error) {
-        console.error(error);
-    } else if (response.statusCode === 200) {
-        const film = JSON.parse(body);
-        const characters = film.characters;
-
-        characters.forEach(characterUrl => {
-            request.get(characterUrl, (err, res, body) => {
-                if (err) {
-                    console.error(err);
-                } else if (res.statusCode === 200) {
-                    const character = JSON.parse(body);
-                    console.log(character.name);
-                } else {
-                    console.error(`Failed to fetch character details. Status code: ${res.statusCode}`);
-                }
-            });
-        });
-    } else {
-        console.error(`Failed to fetch movie details. Status code: ${response.statusCode}`);
-    }
+  if (error) {
+    console.error(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request.get(character, (error, response, body) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
 });

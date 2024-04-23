3ac86c8c6da52e1513b72e7ca674a3539b186f6b
Fatma <fatmacivil@hotmail.com>
2024-04-23 23:14:58 +0300

    ff

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,23 +1,34 @@
 #!/usr/bin/node
 
+const args = process.argv;
 const request = require('request');
 
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${args[2]}`;
 
-request.get(url, (error, response, body) => {
+request(url, async (error, response, body) => {
   if (error) {
     console.error(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.error(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
+    return;
   }
+
+  const moviesCharacters = JSON.parse(body).characters;
+
+  const names = [];
+  for (const character of moviesCharacters) {
+    try {
+      const characterData = await new Promise((resolve, reject) => {
+        request(character, (error, response, body) => {
+          if (error) reject(error);
+          else resolve(JSON.parse(body).name);
+        });
+      });
+      names.push(characterData);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  names.forEach((name) => {
+    console.log(name);
+  });
 });
